66f1edd5fc22749ac8c1696ab3f8f9e469d709b3
Abhishek Abbi <abhishekabbi.work@gmail.com>
2024-12-14 16:07:35 +0530

    Update spacebutton.tsx

--- src/components/ui/spacebutton.tsx
+++ src/components/ui/spacebutton.tsx
@@ -1,4 +1,4 @@
-import { ArrowRightFromLineIcon, ArrowRightToLine } from 'lucide-react';
+import { ArrowRightFromLineIcon } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
